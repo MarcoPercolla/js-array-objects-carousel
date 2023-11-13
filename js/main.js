b411a8c7cd2eb3ac1ff05d4cf52803fc3c139207
ReDMallard <marco@titori.it>
2023-11-13 23:58:01 +0100

    ordina corregge  e aggiunge click su miniature

--- js/main.js
+++ js/main.js
@@ -1,78 +1,153 @@
-const array2Immagini = [
-    "img/01.webp",
-    "img/02.webp",
-    "img/03.webp",
-    "img/04.webp",
-    "img/05.webp",
+const arrayImmagini = [
+    { immagine: "img/01.webp", titolo: "SpiderMan", descrizione: "da grandi poteri blablabla" },
+    { immagine: "img/02.webp", titolo: "immagine n2", descrizione: "blablablablablsbls" },
+    { immagine: "img/03.webp", titolo: "immagine n3", descrizione: "blablablablablsbls" },
+    { immagine: "img/04.webp", titolo: "immagine n4", descrizione: "blablablablablsbls" },
+    { immagine: "img/05.webp", titolo: "immagine n5", descrizione: "blablablablablsbls" },
 ]
-
-let element2Immagini = "";
+// immagine principale
+let elementImmagini = "";
+// miniatura a destra
 let elementMiniImmagini = "";
 
-for (let i = 0; i < array2Immagini.length; i++) {
-    const link2Immagine = array2Immagini[i];
-    element2Immagini += `<img id="midWidth" class="wait wideMid" src="${link2Immagine}">`;
-    elementMiniImmagini += `<img id="miniWidth" class="widemini miniWait" src="${link2Immagine}">`;
-
-}
-document.getElementById("carousel").innerHTML += element2Immagini;
+// utilizzando un forEach ciclo l'array di oggetti e ne tiro fuori i valori che mi servono
+arrayImmagini.forEach((element, index, array) => {
+    console.log(index, element.immagine);
+    const linkImmagine = element.immagine;
+    elementImmagini += `<img id="midWidth" class="wait wideMid" src="${linkImmagine}"><div id="description" class="wait"><h2">${element.titolo}</h2><h3>${element.descrizione}</h3></div>`;
+    elementMiniImmagini += `<img id="miniWidth" class="widemini miniWait" src="${linkImmagine}">`;
+});
+document.getElementById("carousel").innerHTML += elementImmagini;
 document.getElementById("miniSide").innerHTML = elementMiniImmagini;
 
-let current2Img = 0;
+// setto un valore iniziale 0 da associare all'immagine corrente da utilizzare piu tardi per seguire l'immagine corrente con valori tra 0 e array.lenght
+let currentImg = 0;
 const images2 = document.querySelectorAll("#midWidth");
 const miniature = document.querySelectorAll("#miniWidth");
-images2[current2Img].classList.remove("wait");
-images2[current2Img].classList.add("active");
-miniature[current2Img].classList.remove("miniWait");
-miniature[current2Img].classList.add("miniActive");
+const imagesDesctription = document.querySelectorAll("#description");
 
+
+
+// inizializza le classi iniziali per i vari elementi
+settaClassi();
+
+// bottone per andare avanti
 document.getElementById("nextBtn").addEventListener("click", function () {
 
-    if (current2Img < images2.length - 1) {
-        images2[current2Img].classList.remove("active");
-        images2[current2Img].classList.add("wait");
-        miniature[current2Img].classList.add("miniWait");
-        miniature[current2Img].classList.remove("miniActive");
-        current2Img++;
-        images2[current2Img].classList.add("active");
-        images2[current2Img].classList.remove("wait");
-        miniature[current2Img].classList.remove("miniWait");
-        miniature[current2Img].classList.add("miniActive");
+    if (currentImg < images2.length - 1) {
+        scorriAvanti();
+
     } else {
-        images2[current2Img].classList.remove("active");
-        images2[current2Img].classList.add("wait");
-        miniature[current2Img].classList.add("miniWait");
-        miniature[current2Img].classList.remove("miniActive");
-        current2Img = (0);
-        images2[current2Img].classList.add("active");
-        images2[current2Img].classList.remove("wait");
-        miniature[current2Img].classList.remove("miniWait");
-        miniature[current2Img].classList.add("miniActive");
+        scorriAvantiRewind();
 
     }
 });
-
+// bottone per andare indietro
 document.getElementById("prevBtn").addEventListener("click", function () {
 
-    if (current2Img > 0) {
-        images2[current2Img].classList.remove("active");
-        images2[current2Img].classList.add("wait");
-        miniature[current2Img].classList.add("miniWait");
-        miniature[current2Img].classList.remove("miniActive");
-        current2Img--;
-        images2[current2Img].classList.add("active");
-        images2[current2Img].classList.remove("wait");
-        miniature[current2Img].classList.remove("miniWait");
-        miniature[current2Img].classList.add("miniActive");
+    if (currentImg > 0) {
+        scorriIndietro();
     } else {
-        images2[current2Img].classList.remove("active");
-        images2[current2Img].classList.add("wait");
-        miniature[current2Img].classList.add("miniWait");
-        miniature[current2Img].classList.remove("miniActive");
-        current2Img = (images2.length - 1);
-        images2[current2Img].classList.add("active");
-        images2[current2Img].classList.remove("wait");
-        miniature[current2Img].classList.remove("miniWait");
-        miniature[current2Img].classList.add("miniActive");
+        scorriIndietroRewind();
     }
 });
+miniature.forEach((element, index, array) => {
+    element.addEventListener("click", function (){
+        console.log("ok");
+    })
+});
+
+// --------------- funzioni
+
+function scorriIndietro() {
+
+
+    images2[currentImg].classList.remove("active");
+    images2[currentImg].classList.add("wait");
+    miniature[currentImg].classList.add("miniWait");
+    miniature[currentImg].classList.remove("miniActive");
+    imagesDesctription[currentImg].classList.add("wait");
+    imagesDesctription[currentImg].classList.remove("active");
+    currentImg--;
+    images2[currentImg].classList.add("active");
+    images2[currentImg].classList.remove("wait");
+    miniature[currentImg].classList.remove("miniWait");
+    miniature[currentImg].classList.add("miniActive");
+    imagesDesctription[currentImg].classList.remove("wait");
+    imagesDesctription[currentImg].classList.add("active");
+
+
+};
+
+function scorriIndietroRewind() {
+
+
+    images2[currentImg].classList.remove("active");
+    images2[currentImg].classList.add("wait");
+    miniature[currentImg].classList.add("miniWait");
+    miniature[currentImg].classList.remove("miniActive");
+    imagesDesctription[currentImg].classList.add("wait");
+    imagesDesctription[currentImg].classList.remove("active");
+    currentImg = (images2.length - 1);
+    images2[currentImg].classList.add("active");
+    images2[currentImg].classList.remove("wait");
+    miniature[currentImg].classList.remove("miniWait");
+    miniature[currentImg].classList.add("miniActive");
+    imagesDesctription[currentImg].classList.remove("wait");
+    imagesDesctription[currentImg].classList.add("active");
+
+
+};
+
+function scorriAvanti() {
+
+
+    images2[currentImg].classList.remove("active");
+    images2[currentImg].classList.add("wait");
+    miniature[currentImg].classList.add("miniWait");
+    miniature[currentImg].classList.remove("miniActive");
+    imagesDesctription[currentImg].classList.add("wait");
+    imagesDesctription[currentImg].classList.remove("active");
+    currentImg++;
+    images2[currentImg].classList.add("active");
+    images2[currentImg].classList.remove("wait");
+    miniature[currentImg].classList.remove("miniWait");
+    miniature[currentImg].classList.add("miniActive");
+    imagesDesctription[currentImg].classList.remove("wait");
+    imagesDesctription[currentImg].classList.add("active");
+
+
+};
+
+function scorriAvantiRewind() {
+
+
+    images2[currentImg].classList.remove("active");
+    images2[currentImg].classList.add("wait");
+    miniature[currentImg].classList.add("miniWait");
+    miniature[currentImg].classList.remove("miniActive");
+    imagesDesctription[currentImg].classList.add("wait");
+    imagesDesctription[currentImg].classList.remove("active");
+    currentImg = (0);
+    images2[currentImg].classList.add("active");
+    images2[currentImg].classList.remove("wait");
+    miniature[currentImg].classList.remove("miniWait");
+    miniature[currentImg].classList.add("miniActive");
+    imagesDesctription[currentImg].classList.remove("wait");
+    imagesDesctription[currentImg].classList.add("active");
+
+
+};
+
+function settaClassi() {
+
+
+    images2[currentImg].classList.remove("wait");
+    images2[currentImg].classList.add("active");
+    miniature[currentImg].classList.remove("miniWait");
+    miniature[currentImg].classList.add("miniActive");
+    imagesDesctription[currentImg].classList.remove("wait");
+    imagesDesctription[currentImg].classList.add("active");
+
+
+};
